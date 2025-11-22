814221ab0756018af4b655ffed5341f494c0f721
Alberto <a.tiboni@gmail.com>
2025-11-22 18:03:54 +0100

    clean up

--- lib/config/DeviceConfig.h
+++ lib/config/DeviceConfig.h
@@ -112,11 +112,7 @@ namespace CloudMouse
  * - Feature availability (cloud vs local features)
  * - Power management (WiFi radio control)
  */
-<<<<<<< HEAD
 #define WIFI_REQUIRED false
-=======
-#define WIFI_REQUIRED true
->>>>>>> upstream/main
 
 // ============================================================================
 // DEVICE IDENTIFICATION SYSTEM
